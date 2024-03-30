9451ccc02ad3b1f1e7ec9ee158693e296297d302
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-03-30 15:36:37 -0300

    adicionando pagina de formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products.tsx";
+import Contact from "./pages/Contact/Contact.tsx";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route  path="/" element={<Home />} />
           <Route  path="/Produtos" element={<Products />}/>
+          <Route path="/Entre-em-contato" element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
     </>
