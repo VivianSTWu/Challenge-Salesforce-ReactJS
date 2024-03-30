f3bc1f229e8c408af48adc7b69c78d26b771afac
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-03-30 01:54:46 -0300

    substituindo <header/> por <layout/>

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,11 +1,12 @@
 import "./Products.style.ts";
 import Header from "../../components/Header/Header.tsx";
+import { Layout } from "../../components/Layout/Layout.tsx";
 
 
 function Products() {
     return(
        <>
-        <Header/>
+        <Layout/>
        </>
     )
 }
