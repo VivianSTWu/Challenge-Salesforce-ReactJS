d09458f4c8f4031647b1ca37d5575d51dd45a895
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-03-31 20:12:45 -0300

    arrumando o coletor de clicks

--- src/components/ClickRegister/NavigationTracker.tsx
+++ src/components/ClickRegister/NavigationTracker.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ClickerColector = ({ children }) => {
     const [data, setData] = useState([]);
 
+    useEffect(() => {
+        if (data.length > 0) {
+            saveDataToCSV();
+        }
+    }, [data]);
+
     const trackClickAndPosition = (event) => {
         const clickData = {
-            timestamp: new Date().toISOString(),
+            timestamp: new Date().toISOString(), // Timestamp em UTC
             xPosition: event.clientX,
             yPosition: event.clientY
         };
 
         setData(prevData => [...prevData, clickData]);
-
-        saveDataToCSV();
     }
 
     const saveDataToCSV = () => {
@@ -20,25 +24,44 @@ const ClickerColector = ({ children }) => {
         csvContent += "Timestamp,X Position,Y Position\n";
 
         data.forEach(click => {
-            csvContent += click.timestamp + "," + click.xPosition + "," + click.yPosition + "\n";
+            // Converter o horário para o fuso horário local antes de adicionar ao CSV
+            const localTimestamp = new Date(click.timestamp).toLocaleString();
+            csvContent += localTimestamp + "," + click.xPosition + "," + click.yPosition + "\n";
         });
 
- 
         const encodedUri = encodeURI(csvContent);
         const link = document.createElement("a");
         link.setAttribute("href", encodedUri);
         link.setAttribute("download", "navigation_data.csv");
         document.body.appendChild(link);
-
         link.click();
-        
-
         document.body.removeChild(link);
+
+        // Limpar os dados após exportar com sucesso
+        setData([]);
     }
 
+    const handleElementClick = (event) => {
+        trackClickAndPosition(event);
+    }
+
+    const handleElementMouseDown = (event) => {
+        trackClickAndPosition(event);
+    }
+
+    const childrenWithProps = React.Children.map(children, child => {
+        if (React.isValidElement(child) && !child.props.onClick && !child.props.onMouseDown) {
+            return React.cloneElement(child, {
+                onClick: handleElementClick,
+                onMouseDown: handleElementMouseDown
+            });
+        }
+        return child;
+    });
+
     return (
-        <div style={{}} onClick={trackClickAndPosition}>
-            {children}
+        <div>
+            {childrenWithProps}
         </div>
     );
 }
