088fb2a8041fc9e3e6ccc3d570ae34e3a69dc7a5
VivianSTWu <vivian.stwu@gmail.com>
2024-03-31 18:23:30 -0300

    Add widget acessibilidade, mas meio bugado

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,37 @@ import Einstein from "./pages/Product-Einstein-GPT/Einstein.tsx";
 import SalesCloud from "./pages/SalesCloud/SalesCloud.tsx";
 import CRM from "./pages/CRM/CRM.tsx";
 
+import { Fab, Action } from 'react-tiny-fab';
+import 'react-tiny-fab/dist/styles.css'
+import { useState } from "react";
+
 
 function App() {
+
+  const HandleClickProt = () => { document.body.classList.toggle("protanopia") }
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route  path="/" element={<Home />} />
-          <Route  path="/Produtos" element={<Products />}/>
-          <Route path="/Entre-em-contato" element={<Contact/>}/>
-          <Route  path="/Salesforce" element={<Salesforce />}/>
-          <Route path="/Einstein-gpt" element={<Einstein/>}/>
-          <Route path= "/Salescloud" element={<SalesCloud/>}/>
-          <Route path= "/CRM" element={<CRM/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/Produtos" element={<Products />} />
+          <Route path="/Entre-em-contato" element={<Contact />} />
+          <Route path="/Salesforce" element={<Salesforce />} />
+          <Route path="/Einstein-gpt" element={<Einstein />} />
+          <Route path="/Salescloud" element={<SalesCloud />} />
+          <Route path="/CRM" element={<CRM />} />
         </Routes>
       </BrowserRouter>
+
+      <Fab icon={<span>+</span>} mainButtonStyles={{ backgroundColor: '#e74c3c' }}>
+        {/*<Action text="Add Something" style={{ backgroundColor: '#3498db' }} onClick={() => alert('It works!')}>
+          &plus;
+  </Action>*/}
+        <Action text="Protanopia" style={{ backgroundColor: '#3498db' }} onClick={HandleClickProt} >
+          =
+        </Action>
+      </Fab>
     </>
   )
 }
